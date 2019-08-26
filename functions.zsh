c5a4a8f98891cbe205edc30a4e7b5ec3bba93604
Alex MacArthur <alex@macarthur.me>
2019-08-26 14:10:46 -0500

    Update clc function.

--- functions.zsh
+++ functions.zsh
@@ -1,25 +1,25 @@
-# Copy the last commit in a branch.
-# If a branch name is passed as an argument, get last commit from that branch.
+# Copy the last commit in a branch. 
+# Passing a branch name will checkout that branch, copy the last commit, and return to previous branch.
 function clc {
     RED="\033[0;31m"
     GREEN="\033[0;32m"
     RESET="\033[0m"
 
-    function copyLastCommit {
+    function copy_last_commit {
         BRANCH=$(git rev-parse --abbrev-ref HEAD)
         LAST_COMMIT_SHA=$(git rev-parse HEAD)
-        echo "$LAST_COMMIT_SHA" | pbcopy
+        echo "$LAST_COMMIT_SHA" | tr -d '\n' | pbcopy
         echo "Copied ${GREEN}${LAST_COMMIT_SHA} ${RESET}from the ${BRANCH} branch."
     }
 
     if [ ! -z "$1" ]; then
         if git checkout $1 >/dev/null; then
-            copyLastCommit
+            copy_last_commit
             git checkout - >/dev/null
         else
             echo "${RED}Checkout failed. Did not copy anything."
         fi
     else
-        copyLastCommit
+        copy_last_commit
     fi
 }
